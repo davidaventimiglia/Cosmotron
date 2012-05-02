a1257b12eee6d8e332c648f395689e0676a3f6de
David A. Ventimiglia <dventimi@gmail.com>
2012-05-02 09:27:55 -0700

    Added scripts to set the environment and to run atomic and to run cosmotron

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,6 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((java-mode
-  (compile-command . "cd ~/work/atomic ; make")))
+  (compile-command . (substitute-env-vars "cd $ATOMIC_HOME; make"))))
 
 
