24dd66e787a0dde4b676d573c6372dae601c79cc
David A. Ventimiglia <dventimi@gmail.com>
2013-02-20 19:53:48 -0800

    Removed setenv.sh, update .dir-locals.el, and updated the README file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((java-mode
-  (compile-command . (substitute-env-vars "cd $ATOMIC_HOME; make"))))
-
-
+((nil
+  (compile-command . (concat "cd " 
+			     (let ((l (dir-locals-find-file (or (buffer-file-name) default-directory)))) 
+			       (if (listp l) (car l) l)) 
+			     " ; make"))))
