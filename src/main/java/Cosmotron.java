29c6d2e37956267851c1c510db99461753345c00
David A. Ventimiglia <dventimi@gmail.com>
2014-11-20 06:12:13 -0800

    Removed a lot of stuff

--- src/main/java/Cosmotron.java
+++ src/main/java/Cosmotron.java
@@ -1,46 +1,28 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Arrays;
-// import joptsimple.OptionParser;
-// import joptsimple.OptionSet;
-// import joptsimple.OptionSpec;
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.Wrapper;
-import org.apache.catalina.startup.Tomcat;
-// import org.eclipse.jetty.server.Server;
-// import org.eclipse.jetty.servlet.DefaultServlet;
-// import org.eclipse.jetty.servlet.ServletContextHandler;
-// import org.eclipse.jetty.servlet.ServletHolder;
-// import org.eclipse.jetty.util.log.Log;
-// import org.eclipse.jetty.util.log.Logger;
+import java.io.*;
+import java.util.*;
+import org.apache.catalina.*;
+import org.apache.catalina.deploy.*;
+import org.apache.catalina.startup.*;
 
 public class Cosmotron {
-    public static void main (String[] args) throws Exception{
-	// Logger logger = Log.getLogger(Cosmotron.class);
+    public static void main (String[] args) throws Exception {
 	try {
-	    // String cosmotronData = options.valueOf(data);
-	    // String contextPath = options.valueOf(context);
-	    // Integer webPort = options.valueOf(port);
-	    // logger.info("data-directory = " + cosmotronData);
-	    // logger.info("port-number = " + webPort);
-	    // logger.info("context-path = " + contextPath);
-	    // STGroup group = new STGroupFile(cosmotronData + "/templates/cosmotron_splash.stg");
+	    String atomicUrl = System.getProperty("atomic-url");
+	    String httpPort = System.getProperty("http-port");
+	    String contextPath = System.getProperty("context-path");
+	    System.out.println("atomic-url = " + atomicUrl);
+	    System.out.println("port-number = " + httpPort);
+	    System.out.println("context-path = " + contextPath);
+	    System.out.println(new Scanner(ClassLoader.getSystemResourceAsStream("cosmotron_splash.txt")).useDelimiter("\\Z").next().replace("$PORT$", httpPort));
 	    Tomcat tomcat = new Tomcat();
-	    // tomcat.setPort(webPort);
-	    // Context ctx = tomcat.addContext("/"+contextPath, new File(".").getAbsolutePath());
-	    // Wrapper w = Tomcat.addServlet(ctx, "cosmotron", new CosmotronServlet());
-	    // ctx.addServletMapping("/*", "cosmotron");
-	    // w.addInitParameter(CosmotronServlet.COSMOTRONDATA, cosmotronData + "/templates");
+	    tomcat.setPort(Integer.parseInt(httpPort));
+	    Context ctx = tomcat.addContext(contextPath, new File(".").getAbsolutePath());
+            ContextResource resource = new ContextResource();
+	    Wrapper w = Tomcat.addServlet(ctx, "cosmotron", new CosmotronServlet());
+	    ctx.addServletMapping("/*", "cosmotron");
 	    tomcat.start();
-	    // System.out.println(group.getInstanceOf("logo_Standard").render());
-	    // ST r = group.getInstanceOf("ready");
-	    // r.add("port", webPort);
-	    // System.out.println(r.render());
-	    tomcat.getServer().await();	}
+	    tomcat.getServer().await();}
 	catch (Exception t) {
-	    t.printStackTrace();
-	    // logger.warn(t.getLocalizedMessage());
-	    // logger.warn("Cosmotron has encountered a fatal error and is shutting down.");
+	    t.printStackTrace(System.err);
+	    System.out.println("Cosmotron has encountered a fatal error and is shutting down.");
 	    System.exit(1);}}}
