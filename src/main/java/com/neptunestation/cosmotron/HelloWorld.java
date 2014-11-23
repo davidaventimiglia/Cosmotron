a27c6ec7a379fd533d5a1c0eff3fc4f7b1aff43c
David A. Ventimiglia <dventimi@gmail.com>
2014-11-22 18:22:27 -0800

    Rudiments of a simple tag library taking shape

---
+++ src/main/java/com/neptunestation/cosmotron/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.neptunestation.cosmotron;
+
+import java.util.*;
+
+public class HelloWorld {
+    public static String hello ( String text ) {
+        return "Hello, World!";}}
+
---
+++ src/main/java/com/neptunestation/cosmotron/OdataReader.java
@@ -0,0 +1,6 @@
+package org.neptunestation.cosmotron;
+
+public class OdataReader {
+    public static String hello () {
+        return "Hello, World!";}
+}
