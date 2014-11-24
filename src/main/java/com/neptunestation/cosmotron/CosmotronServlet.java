c9dcf3c2885f71a3b3aef6a05de5eb5999a28680
dventimi <dventimi@dventimi-EB1501>
2014-11-24 06:15:27 -0800

    Trying out request dispatching from servlets to JSPs

--- src/main/java/com/neptunestation/cosmotron/CosmotronServlet.java
+++ src/main/java/com/neptunestation/cosmotron/CosmotronServlet.java
@@ -1,8 +1,9 @@
 package com.neptunestation.cosmotron;
 
 import java.io.*;
+import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class CosmotronServlet extends HttpServlet {
-    @Override public void doGet (HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.getWriter().write("Hello, World!");}}
+    @Override public void doGet (HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, res);}}
