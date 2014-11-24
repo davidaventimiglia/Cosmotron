89d595f8fcb75bea3129b155dfe55efbcb5dd840
dventimi <dventimi@dventimi-EB1501>
2014-11-24 05:06:33 -0800

    Fixed up the tag to take a parameter.

--- src/main/java/com/neptunestation/cosmotron/HelloWorld.java
+++ src/main/java/com/neptunestation/cosmotron/HelloWorld.java
@@ -3,6 +3,6 @@ package com.neptunestation.cosmotron;
 import java.util.*;
 
 public class HelloWorld {
-    public static String hello ( String text ) {
-        return "Hello, World!";}}
+    public static String hello (String text) {
+        return String.format("Hello, %s!", text);}}
 
