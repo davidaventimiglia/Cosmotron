ec1e92773e096787e80d44276b800f3776a11dcb
dventimi <dventimi@workstation>
2014-12-22 05:21:11 -0800

    Moving the furniture

--- src/main/java/com/neptunestation/cosmotron/OdataReader.java
+++ src/main/java/com/neptunestation/cosmotron/OdataReader.java
@@ -1,5 +1,13 @@
 package org.neptunestation.cosmotron;
 
+import java.util.*;
+import javax.xml.bind.*;
+import org.apache.olingo.odata2.api.commons.*;
+import org.apache.olingo.odata2.api.edm.*;
+import org.apache.olingo.odata2.api.ep.*;
+import org.apache.olingo.odata2.api.ep.feed.*;
+import org.apache.olingo.odata2.api.exception.*;
+
 public class OdataReader {
     public static String hello () {
         return "Hello, World!";}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,8 @@
 <%@ page contentType="text.html; charset=UTF-8"%>
 <%@ page pageEncoding="UTF-8"%>
+<%@ page import="org.apache.olingo.odata2.api.edm.*"%>
 <%@ taglib prefix="my" uri="http://www.neptunestation.com/cosmotron-tags"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
   <head>
     <title>Cosmotron</title>
